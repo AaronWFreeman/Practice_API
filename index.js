bea79328a7626e0132b1cca8fcea64d6da347fbb
Aaron Freeman <Freeman0483@gmail.com>
2018-02-26 13:31:21 -0800

    GET

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const apiURL ='https://api.lyrics.ovh/v1/';
 
-function getDataFromApi(artist, title, callback) {
-  let query = {
-    q: artist + '/' + title,
-    language: 'en-us',
-  }
-  let autoSettings = {
-    url: apiURL + query.q,
-    data: query,
-    dataType: 'json',
-    type: 'get',
-    success: callback
-  }
-  $.ajax(autoSettings);
+function getDataFromApi(artist, title) {
+  let url = apiURL + artist + '/' + title;
+  fetch(url, {
+      method: 'GET',
+      headers: {"Accept": "application/json"},
+      mode: 'cors'
+    }).then((response) => {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+        return response.json();
+    }).then((data) => {
+        return data;
+    }).catch(function(err) {
+      console.log('goFetch() Error :-S --> utils.js', err);
+      return err;
+    });
+  // let query = {
+  //   q: artist + '/' + title,
+  //   language: 'en-us',
+  // }
+  // let autoSettings = {
+  //   url: apiURL + query.q,
+  //   data: query,
+  //   dataType: 'json',
+  //   type: 'get',
+  //   success: callback
+  // }
+  // $.ajax(autoSettings);
   // console.log(query, autoSettings);
 
 }
@@ -37,7 +55,7 @@ function watchSubmit() {
     event.preventDefault();
     let artist = $('.js-query-artist').val();
     let title = $('.js-query-title').val();
-    getDataFromApi(artist, title, displaySearchData);
+    getDataFromApi(artist, title);
   });
 }
 
