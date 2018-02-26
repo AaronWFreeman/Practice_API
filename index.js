fe77eee0dc40ee4bc6877278354c4824f122446b
Aaron Freeman <Freeman0483@gmail.com>
2018-02-26 11:00:55 -0800

    Practice API

---
+++ index.js
@@ -0,0 +1,44 @@
+const apiURL ='https://lyricsovh.docs.apiary.io/#';
+
+function getDataFromApi(artist, title, callback) {
+  let query = {
+    q: artist + " " + title,
+    language: 'en-us',
+  }
+  let autoSettings = {
+    url: apiURL + query,
+    data: query,
+    dataType: 'json',
+    type: 'get',
+    success: callback
+  }
+  $.ajax(query, autoSettings);
+  console.log(query, autoSettings);
+
+}
+
+function renderLyrics(lyricObj) {
+  // return
+  //   <div>
+  //     <p>
+  //       // `${lyricObj}`
+  //     </p>
+  //   </div>;
+}
+
+function displaySearchData(data) {
+  const results = data.items.map((item, index) => renderLyrics(item));
+  $('js-search-results').html(results);
+  console.log(data);
+}
+
+function watchSubmit() {
+  $('.js-search-button').click(event => {
+    event.preventDefault();
+    let artist = $('.js-query-artist').val();
+    let title = $('.js-query-title').val();
+    getDataFromApi(artist, title, displaySearchData);
+  });
+}
+
+$(watchSubmit);
